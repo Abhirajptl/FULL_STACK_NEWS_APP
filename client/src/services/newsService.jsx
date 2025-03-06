c6d4b7859e564428ea2bad4b1ec6261e044348d3
Abhiraj Patel <patelabhiraj222@gmail.com>
2025-03-06 18:13:33 +0530

    const API_URL = import.meta.env.VITE_SERVER_URL;

--- client/src/services/newsService.jsx
+++ client/src/services/newsService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/news';
+const API_URL = import.meta.env.VITE_SERVER_URL;
 
 // Fetch all news
 export const fetchNewsAPI = async () => {
